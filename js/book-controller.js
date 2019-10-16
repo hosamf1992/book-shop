caf1b2336d770fbaf4e12e0420348a399f764d43
hosamf1992 <56447312+hosamf1992@users.noreply.github.com>
2019-10-16 15:13:13 +0300

    Add files via upload

--- js/book-controller.js
+++ js/book-controller.js
@@ -9,33 +9,39 @@ function onInit() {
 }
 
 function readAndUpdateBook(bookId) {
-    var bookPrice = +prompt('Enter new price!');
+    var bookPrice = +prompt(getTrans('newPrice'));
 
     if (bookPrice !== 0) {
         updateBook(bookId, bookPrice);
         renderTable();
+        doTrans();
+       
     }
 
 }
 
 
 function readAndAddNewBook() {
-    var bookName = prompt('Enter book name!');
-    var price = +prompt('Enter the price');
-    var imgUrl = prompt('Enter image url');
+    var bookName = prompt(getTrans('newBook'));
+    var price = +prompt(getTrans('price'));
+    var imgUrl = prompt(getTrans('url'));
     addBook(bookName, price, imgUrl);
     renderTable();
+    doTrans();
+
 
 }
 
 
 
 function onDeleteBook(bookId) {
-    var isSure = confirm('Are you sure?')
+    var isSure = confirm(getTrans('sure'))
     if (!isSure) return;
 
     deleteBook(bookId);
     renderTable();
+    doTrans();
+
 }
 
 
@@ -43,7 +49,7 @@ function renderTable() {
 
     var strHtml = '';
     var headerTbHtml = '';
-    headerTbHtml = `<tr> <th>Id</th>  <th class='cursor' onclick="onSetFilter('Name')" >Title</th> <th class='cursor' onclick="onSetFilter('Price')"">Price</th> <th>Image</th>  <th colspan='3'>Actions</th> </tr>`;
+    headerTbHtml = `<tr> <th data-trans="table-id">Id</th>   <th data-trans="table-title" class='cursor' onclick="onSetFilter('Name')" >Title</th> <th data-trans="table-price" class='cursor' onclick="onSetFilter('Price')"">Price</th> <th data-trans="table-img">Image</th>  <th data-trans="table-actions" colspan='3'>Actions</th> </tr>`;
     strHtml += '<tr>';
 
     gBook.forEach(function (book) {
@@ -51,9 +57,9 @@ function renderTable() {
         strHtml += `<td> ${book.name} </td>`;
         strHtml += `<td> ${book.price} </td>`;
         strHtml += `<td> <img src="${book.imgUrl}" width="40"> </td>`;
-        strHtml += `<td> <button class='btn read-btn' onclick="onReadBook(${book.id})"> Read</button> </td>`;
-        strHtml += `<td> <button class='btn update-btn' onclick="readAndUpdateBook(${book.id})"> Update</button> </td>`;
-        strHtml += `<td> <button class='btn delete-btn' onclick="onDeleteBook(${book.id})"> Delete</button> </td>`;
+        strHtml += `<td> <button data-trans="read-btn" class='btn read-btn' onclick="onReadBook(${book.id})"> Read</button> </td>`;
+        strHtml += `<td> <button data-trans="update-btn" class='btn update-btn' onclick="readAndUpdateBook(${book.id})"> Update</button> </td>`;
+        strHtml += `<td> <button data-trans="delete-btn" class='btn delete-btn' onclick="onDeleteBook(${book.id})"> Delete</button> </td>`;
         strHtml += '</tr>';
 
     });
@@ -99,7 +105,7 @@ function closeModal() {
 }
 
 function onSetFilter(filterBy) {
-   
+
     console.log('Setting Filter', filterBy);
     setFilter(filterBy);
     if (filterBy === 'Price') sortByPrice();
@@ -108,5 +114,19 @@ function onSetFilter(filterBy) {
     renderTable();
 
 
+
+}
+
+function onSetLang(lang) {
+    setLang(lang);
+    if (lang === 'he') {
+        document.body.classList.add('rtl')
+      
+       
+    } else {
+        document.body.classList.remove('rtl')
+    }
+    
+    doTrans();
 
 }
---
+++ js/i18n.service.js
@@ -0,0 +1,140 @@
+'use strict'
+
+
+
+var gTrans = {
+    'main-title': {
+        en: 'Welcome to my bookshop',
+
+        he: 'ברוכים הבאים לחנות ספרים שלי'
+    },
+    'filter-msg': {
+        en: '  Click the Title or Price to sort',
+
+        he: 'סנן את הטבלה לפי כותרת או מחיר '
+    },
+
+    'add-btn': {
+        en: 'Add new book',
+
+        he: 'הוסף ספר חדש',
+    }, 'table-id': {
+        en: 'Id',
+
+        he: ' מספר',
+    },
+
+    'table-title': {
+        en: 'Title',
+        he: 'שם ספר',
+    },
+
+    'table-price': {
+        en: 'Price',
+
+        he: 'מחיר',
+    },
+    'table-img': {
+        en: 'Image',
+
+        he: 'תמונה',
+    },
+    'table-actions': {
+        en: 'Actions',
+
+        he: 'פעולות',
+    },
+    'read-btn': {
+        en: 'Read',
+        he: 'פתח',
+    }, 'update-btn': {
+        en: 'Update',
+
+        he: 'עדכן',
+    }, 'delete-btn': {
+        en: 'Delete',
+
+        he: 'מחק',
+    },
+    'modal-title': {
+        en: 'Title',
+        he: 'שם ספר'
+    },
+    'modal-price': {
+        en: 'Price',
+        he: ' מחיר'
+    },
+    'modal-rate': {
+        en: 'Rate',
+        he: ' דירוג'
+    },
+    'modal-change': {
+        en: 'Change-rate',
+        he: ' ערוך דירוג'
+    },
+    'modal-btn': {
+        en: 'Submit',
+        he: ' ערוך '
+    },
+    'modal-btn': {
+        en: 'Submit',
+        he: ' ערוך '
+    },
+    sure: {
+        en: 'Are you sure?',
+        he: 'האם אתה בטוח?',
+    },
+
+    newPrice: {
+        en: 'Enter new price!',
+        he: 'הזן מחיר חדש!',
+    },
+    newBook: {
+        en: 'Enter book name!',
+        he: 'הזן שם ספר!',
+    },
+    price: {
+        en: 'Enter the price!',
+        he: 'הזן מחיר!',
+    },
+    url: {
+        en: 'Enter image url',
+        he: 'הזן קישור לתמונה'
+    }
+
+
+
+}
+
+var gCurrLang = 'en';
+
+
+function doTrans() {
+    var els = document.querySelectorAll('[data-trans]');
+
+    for (var i = 0; i < els.length; i++) {
+        var el = els[i];
+        var transKey = el.dataset.trans;
+
+        var txt = getTrans(transKey);
+        el.innerText = txt;
+
+
+    }
+}
+
+function getTrans(transKey) {
+    var keyTrans = gTrans[transKey];
+    if (!keyTrans) return 'UNKNOWN';
+
+    var txt = keyTrans[gCurrLang];
+
+    // If not found - use english
+    if (!txt) txt = keyTrans['en'];
+
+    return txt;
+}
+
+function setLang(lang) {
+    gCurrLang = lang;
+}
